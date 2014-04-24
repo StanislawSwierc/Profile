eb6ffcec0859353ea86c145fd4ac5a8f4ffa87ed
Stanislaw Swierc <stanislaw.swierc@gmail.com>
2014-04-24 09:47:55 +0100

    Add .latexmkrc file

--- install.bat
+++ install.bat
@@ -4,3 +4,4 @@ mklink %HOME%\_viemurc %HOME%\.home.git\_viemurc
 mklink %HOME%\_vimperatorrc %HOME%\.home.git\_vimperatorrc
 mklink %HOME%\_vimrc %HOME%\.home.git\_vimrc
 mklink %HOME%\_vsvimrc %HOME%\.home.git\_vsvimrc
+mklink %HOME%\.latexmkrc %HOME%\.home.git\.latexmkrc
