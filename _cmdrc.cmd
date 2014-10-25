9249064f65d0abad06e648bc592d6f8fceded306
Stanislaw Swierc <stansw@microsoft.com>
2014-10-25 09:45:54 +0100

    Add vscmd and vscmd14 commands to the _cmdrc

--- _cmdrc.cmd
+++ _cmdrc.cmd
@@ -7,4 +7,8 @@ doskey tg=targit
 doskey cds=cd C:\Data\Repositories
 doskey cdd=cd C:\Data\Development
 doskey cdh=cd %USERPROFILE%
- 
+
+doskey vscmd14=%COMSPEC% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" & echo Welcome to Microsoft Visual Studio 14.0 Command Prompt"
+doskey vscmd=%COMSPEC% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" & echo Welcome to Microsoft Visual Studio 14.0 Command Prompt"
+
+
