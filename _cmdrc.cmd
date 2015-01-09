c8dab3a4894c970fd0b44cb8ef88c80f1a326aef
Stanislaw Swierc <stansw@microsoft.com>
2015-01-09 21:03:54 +0000

    Creat up, upp and uppp macros

--- _cmdrc.cmd
+++ _cmdrc.cmd
@@ -8,6 +8,12 @@ doskey cds=cd C:\Data\Repositories
 doskey cdd=cd C:\Data\Development
 doskey cdh=cd %USERPROFILE%
 
+doskey up=cd ..
+doskey upp=cd ..\..
+doskey uppp=cd ..\..\..
+doskey upppp=cd ..\..\..\..
+
+
 doskey vscmd14=%COMSPEC% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" & echo Welcome to Microsoft Visual Studio 14.0 Command Prompt"
 doskey vscmd=%COMSPEC% /k ""C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\Tools\VsDevCmd.bat" & echo Welcome to Microsoft Visual Studio 14.0 Command Prompt"
 
